f64dbc362032faa9e01abf64d848f52808e8b3d7
bumpfuji10 <bumpfuji10@gmail.com>
2026-08-22 16:49:05 +0900

    [add] authentication scaffold and user sign-up
    - Add Rails 8 authentication generator scaffold (sessions, passwords reset)
    - Add User#name column and sign-up flow (users#new/#create)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
-  def index 
+  allow_unauthenticated_access only: %i[ new create ]
+
+  def index
     @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "アカウントを作成しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [:name, :email_address, :password, :password_confirmation])
   end
 end
