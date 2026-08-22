1bbd23f8ce5197209fde71eff64e86bb67d3215c
bumpfuji10 <bumpfuji10@gmail.com>
2026-08-22 16:02:27 +0900

    [add]users schema migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema[8.1].define(version: 2026_08_22_064002) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 end
